3672a7db8231a9bd6431b09e9dc94e181c4e173a
Henry Li <henry5581984@gmail.com>
2020-06-20 17:24:13 -0400

    added canvas.js

---
+++ canvas.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // Resizing
+    canvas.height = window.innerHeight - 8;
+    canvas.width = window.innerWidth - 8;
+
+    // Variables
+    let painting = false;
+
+    function startPosition(e) {
+        painting = true;
+        // Enable user to draw a dot
+        draw(e);
+    }
+
+    function endPosition() {
+        painting = false;
+        // Avoid auto connection when starting a new line
+        ctx.beginPath();
+    }
+
+    function draw(e) {
+        if(!painting) {
+            return;
+        }
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    // EvenListeners
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", endPosition);
+    canvas.addEventListener("mousemove", draw);
+});
+
+// Resize feature for later version
+window.addEventListener("resize", () => {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+}); 
